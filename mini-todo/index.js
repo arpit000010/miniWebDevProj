7397803c6e15925451233f54d1f00ed893238711
Arpit Yadav <arpit899836@gmail.com>
2026-03-31 23:03:29 +0530

    commiting minii-todo app

---
+++ mini-todo/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let todos = [];
+let nextId = 1;
+
+app.post('/todos', (req, res)=>{
+
+    const {title, description} = req.body;
+
+    if(!title || !description){
+        return res.status(400).json({
+            error: "Title and description are required"
+        });
+    }
+
+    const todo = {
+        id: nextId++,
+        title,
+        description,
+        createdAt: new Date().toISOString()
+    }
+
+    todos.push(todo);
+
+    res.status(201).json(todo);
+
+
+})
+
+app.get('/todos', (req, res) => {
+    res.status(200).json(todos);
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const todo = todos.find((t) => t.id === id);
+
+    if (!todo) {
+        return res.status(404).json({
+            error: "Todo not found"
+        });
+    }
+
+    res.status(200).json(todo);
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
